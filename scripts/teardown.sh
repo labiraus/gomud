d3b62348e941cf1984bd1eb3aabd6caa5fc418e4
Oliver Hathaway <oliver.hathaway@bjss.com>
2021-04-12 21:54:56 +0100

    Added in database creation

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 docker stop local-dev-control-plane
-docker rm local-dev-control-plane 
-
-read -p "Hit any key to end"
+docker rm local-dev-control-plane
--- sql/fill-database.sql
+++
@@ -1,8 +0,0 @@
-SET IDENTITY_INSERT gomud..rooms ON
-INSERT INTO gomud..rooms (id, [name], [description]) VALUES 
-(1, 'The Beginning', 'This is where everything starts from'),
-(2, 'The End', 'You have reached the end of the world')
-SET IDENTITY_INSERT gomud..rooms OFF
-
-INSERT INTO gomud..connections (room_id, neighbour_id, dir, two_way) VALUES
-(1, 2, 2, 1)
